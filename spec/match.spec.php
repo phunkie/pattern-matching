365970ee6c7835c910a8b04fbc31cfb31465c274
Marcello Duarte <marcello.duarte@gmail.com>
2017-07-24 09:18:33 +0100

    Add deconstructor

--- spec/match.spec.php
+++ spec/match.spec.php
@@ -1,47 +1,105 @@
 <?php
 
-use function \Phunkie\Lang\PatternMatching\match;
-use const \Phunkie\Lang\PatternMatching\_;
+namespace Referenced {
 
-describe("match", function() {
-    context("behaving like a switch", function() {
-        it("matches a simple value with a condition", function () {
-            $x = 1;
-            $y = null;
+    use DeconstructionExample;
+    use Phunkie\Lang\PatternMatching\Deconstructor;
 
-            $on = match($x); switch(true) {
-                case $on(1): $y = "one"; break;
-            }
+    function DeconstructionExample(&$x, &$y)
+    {
+        return new Deconstructor(DeconstructionExample::class, $x, $y);
+    }
+}
 
-            expect($y)->toBe("one");
+namespace {
+
+    use function Phunkie\Lang\PatternMatching\match;
+    use const Phunkie\Lang\PatternMatching\_;
+    use function Referenced\DeconstructionExample as DeconstructionExampleCase;
+
+    class DeconstructionExample
+    {
+        private $x;
+        private $y;
+
+        public function __construct($x, $y)
+        {
+            $this->x = $x;
+            $this->y = $y;
+        }
+    }
+
+    describe("Pattern matching", function () {
+        context("Switch behaviour", function () {
+            it("matches a simple value with a condition", function () {
+                $x = 1;
+                $y = null;
+
+                $on = match($x);
+                switch (true) {
+                    case $on(1):
+                        $y = "one";
+                        break;
+                }
+
+                expect($y)->toBe("one");
+            });
+
+
+            it("matches one in multiple cases", function () {
+                $x = 1;
+                $y = null;
+
+                $on = match($x);
+                switch (true) {
+                    case $on(1):
+                        $y = "one";
+                        break;
+                    case $on(2):
+                        $y = "two";
+                        break;
+                    case $on(3):
+                        $y = "three";
+                        break;
+                }
+
+                expect($y)->toBe("one");
+            });
+
+
+            it("matches one in multiple cases with default clause", function () {
+                $x = 4;
+                $y = null;
+
+                $on = match($x);
+                switch (true) {
+                    case $on(1):
+                        $y = "one";
+                        break;
+                    case $on(2):
+                        $y = "two";
+                        break;
+                    case $on(_):
+                        $y = "many";
+                        break;
+                }
+
+                expect($y)->toBe("many");
+            });
         });
+        context("Deconstruction", function () {
 
-
-        it("multiple cases", function () {
-            $x = 1;
-            $y = null;
-
-            $on = match($x); switch(true) {
-                case $on(1): $y = "one"; break;
-                case $on(2): $y = "two"; break;
-                case $on(3): $y = "three"; break;
-            }
-
-            expect($y)->toBe("one");
-        });
-
-
-        it("multiple cases with default clause", function () {
-            $x = 4;
-            $y = null;
-
-            $on = match($x); switch(true) {
-                case $on(1): $y = "one"; break;
-                case $on(2): $y = "two"; break;
-                case $on(_): $y = "many"; break;
-            }
-
-            expect($y)->toBe("many");
+            it("deconstructs classes", function () {
+                $example = new DeconstructionExample(42, "Chuck");
+                $on = match($example);
+                switch (true) {
+                    case $on(DeconstructionExampleCase($a, $b)):
+                        expect($a)->toBe(42);
+                        expect($b)->toBe("Chuck");
+                        return;
+                }
+                throw new \Exception("Not deconstructed correctly");
+            });
         });
     });
-});
+}
--- src/match.php
+++ src/match.php
@@ -1,9 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Phunkie\Lang\PatternMatching {
     const _ = "Phunkie\\Lang\\PatternMatching::_";
 
-    function match(...$values)
+    function match(...$values): Match
     {
         return new Match(...$values);
     }
@@ -13,13 +13,43 @@ namespace Phunkie\Lang\PatternMatching {
         switch(true):
             case $condition === _:
             case sameTypeSameValue($condition, $value):
+            case matchByReference($condition, $value):
                 return true;
         endswitch;
         return false;
     }
 
-    function sameTypeSameValue($condition, $value)
+    function sameTypeSameValue($condition, $value): bool
     {
         return gettype($condition) == gettype($value) && $value == $condition;
+    }
+
+    function matchByReference($condition, $value): bool
+    {
+        if ($condition instanceof Deconstructor) {
+            return matchDeconstructor($condition, $value, $condition->class);
+        }
+        return false;
+    }
+
+    function matchDeconstructor($condition, $object, $class): bool
+    {
+        if ($condition instanceof Deconstructor && is_object($object) && get_class($object) === $class) {
+            $reflected = new \ReflectionClass($object);
+            $parameters = $reflected->getConstructor()->getParameters();
+            for ($i = 1; $i <= count($parameters); $i++) {
+                if (!$reflected->hasProperty($parameters[$i - 1]->getName())) {
+                    throw new \Error("To use generic pattern matching you have to name the constructor argument as you ".
+                        "have named the class property");
+                }
+                if (isset(((array) $object)["\0$class\0{$parameters[$i - 1]->getName()}"])) {
+                    $condition->{"_$i"} = ((array)$object)["\0$class\0{$parameters[$i - 1]->getName()}"];
+                } elseif (isset(((array)$object)["{$parameters[$i - 1]->getName()}"])) {
+                    $condition->{"_$i"} = ((array)$object)["{$parameters[$i - 1]->getName()}"];
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
